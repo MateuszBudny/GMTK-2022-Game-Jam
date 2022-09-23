75cfa49a339c64a61566367ede0a8b9a8277f123
Mateusz Budny <mateusz.budny97@gmail.com>
2022-09-23 21:05:43 +0200

    Bombs random, downward and back forces added on falling.

--- Assets/_GMTK/Scripts/Bomb.cs
+++ Assets/_GMTK/Scripts/Bomb.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    [SerializeField]
+    private float backForceWhenFalling;
+    [SerializeField]
+    private Vector3 minRandomRotationForceOnFall;
+    [SerializeField]
+    private Vector3 maxRandomRotationForceOnFall;
+    [SerializeField]
+    private float minTipDownRotationForceOnFall;
+    [SerializeField]
+    private float maxTipDownRotationForceOnFall;
+
     private Rigidbody rigid;
     private Vector3 worldPositionArmed;
+    private Quaternion worldRotationArmed;
 
     private void Awake()
     {
@@ -15,18 +27,27 @@ public class Bomb : MonoBehaviour
     void Start()
     {
         worldPositionArmed = transform.position;
+        worldRotationArmed = transform.rotation;
     }
     
     public void ResetPos()
     {
         rigid.isKinematic = true;
         transform.position = worldPositionArmed;
+        transform.rotation = worldRotationArmed;
     }
 
     public void Fall()
     {
         rigid.isKinematic = false;
         transform.SetParent(null);
+
+        Vector3 randomRotationForce = minRandomRotationForceOnFall.RandomRange(maxRandomRotationForceOnFall);
+        float tipDownRotationForce = Random.Range(minTipDownRotationForceOnFall, maxTipDownRotationForceOnFall);
+        rigid.AddForce(new Vector3(0f, 0f, backForceWhenFalling), ForceMode.Force);
+        rigid.AddTorque(randomRotationForce, ForceMode.Force);
+        rigid.AddTorque(-new Vector3(tipDownRotationForce, 0f, 0f), ForceMode.Force);
+
         SoundManager.Instance.Play(Audio.BombsFalling); // play for every bomb or just once for them all?
     }
 }
