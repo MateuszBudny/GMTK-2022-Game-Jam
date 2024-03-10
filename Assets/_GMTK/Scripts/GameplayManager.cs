f4caccc85d8b7f1f9a83d91d4d5e27faaae1869e
Mateusz Budny <mateusz.budny97@gmail.com>
2024-03-11 00:09:32 +0100

    Throwing satan dices on black screen fading in fixed (PlayerIsSatan state added).

--- Assets/_GMTK/Scripts/GameplayManager.cs
+++ Assets/_GMTK/Scripts/GameplayManager.cs
@@ -33,7 +33,8 @@ public class GameplayManager : SingleBehaviour<GameplayManager>
     public BehaviourTree playerCanInteractNoDiceMustDropBombsState;
     public BehaviourTree playerIsDroppingBombsState;
     public BehaviourTree playerBecomesSatanState;
-    public BehaviourTree satanIsWreckingHavoc;
+    public BehaviourTree playerIsSatanState;
+    public BehaviourTree satanIsWreckingHavocState;
     public BehaviourTree gameOverState;
 
     [Header("Signals")]
